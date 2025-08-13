7a4b0e89e482b22b8cde7fa2e1e1b9be51928294
Vishnu <vishnub195@gmail.com>
2025-08-13 17:48:48 +0530

    Adding Project Documentation File and changed the parameters of similarity search to k=10

--- src/similarity_checks.py
+++ src/similarity_checks.py
@@ -6,7 +6,7 @@ vectorstore = FAISS.load_local("faiss_job_index_miniLM", embedding_model, allow_
 
 def similarity_checking(vectorestore,resume_embedding):
     # This will return the most similar job documents to your resume
-    search_results = vectorstore.similarity_search_by_vector(resume_embedding, k=6)
+    search_results = vectorstore.similarity_search_by_vector(resume_embedding, k=10)
     print('Similarity Searching Completed') 
     return search_results
 
